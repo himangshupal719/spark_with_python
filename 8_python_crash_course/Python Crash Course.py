90daa16b037a8b74024deb07b2b8168f0ff1486d
himangshupal719 <pal.h719@gmail.com>
2021-10-22 07:03:52 -0700

    Initial Commit - Completed until Python Crash Course

---
+++ 8_python_crash_course/Python Crash Course.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("My name is {}".format('Himangshu'))
+
+
+# In[4]:
+
+
+print("My name is {}, my number is int {}, my number is string {}".format('Himangshu',10, '20'))
+
+
+# In[5]:
+
+
+print("First: {x} Second {y}".format(x='xxx', y='yyy'))
+
+
+# In[6]:
+
+
+print("First: {y} Second {x}".format(x='xxx', y='yyy'))
+
+
+# In[7]:
+
+
+mylist = ['a','b','c']
+
+
+# In[8]:
+
+
+mylist
+
+
+# In[9]:
+
+
+type(mylist)
+
+
+# In[10]:
+
+
+mylist[0]
+
+
+# In[11]:
+
+
+mylist[2]
+
+
+# In[12]:
+
+
+mylist[-1]
+
+
+# In[13]:
+
+
+mylist[0]='new'
+
+
+# In[14]:
+
+
+mylist
+
+
+# In[15]:
+
+
+mylist.append('D')
+
+
+# In[16]:
+
+
+mylist
+
+
+# In[17]:
+
+
+mylist = [1,2,[100, 200]]
+
+
+# In[18]:
+
+
+mylist
+
+
+# In[19]:
+
+
+mylist[2]
+
+
+# In[20]:
+
+
+mylist[2][1]
+
+
+# In[21]:
+
+
+d = {'key1':'value1', 'key2':'value2'}
+
+
+# In[22]:
+
+
+d
+
+
+# In[23]:
+
+
+d['key1']
+
+
+# In[24]:
+
+
+d['key2']
+
+
+# In[25]:
+
+
+print(d)
+
+
+# In[26]:
+
+
+t = (1,2,3)
+
+
+# In[28]:
+
+
+{1,2,3}
+
+
+# In[29]:
+
+
+{11,22,33,11,22,33,44}
+
+
+# In[ ]:
+
+
+
+
